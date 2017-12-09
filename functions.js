a2886ab8d881508503dbdd2ae78a030e5074a20f
Ravern Koh <ravern.koh.dev@gmail.com>
2017-12-09 11:03:13 +0800

    Created structure

---
+++ functions.js
@@ -0,0 +1,4 @@
+{
+  "PRINT": "ITELLHER",
+  "INPUT": "SENDMAN",
+}
---
+++ runtime/index.test.js
@@ -0,0 +1,28 @@
+const test = require('ava');
+const runtime = require('./index');
+
+const functions = require('../functions.json');
+
+test('prints to console correctly', (t) => {
+  const ast = [
+    {
+      value: functions.PRINT,
+      args: [
+        {
+          value: '+',
+          args: [
+            1,
+            2,
+          ],
+        }
+      ],
+    }
+  ];
+
+  const output = stdout.inspectSync(() => {
+    runtime(ast);
+  });
+
+  console.log(output);
+  t.pass();
+});
