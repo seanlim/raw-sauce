db5e4eed87a42e936466c5d9495f934f75f347aa
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2017-12-09 12:02:41 +0800

    done test

--- parser/index.test.js
+++ parser/index.test.js
@@ -1,16 +1,20 @@
 const test = require('ava');
 const runtime = require('./index');
 
+const tokens = require('../tokens');
+
 test('skiddy pap boom parser', (t) => {
   // Input
   const input = [
-    ['IDEN', 'PRINT'],
-    ['OPEN_PAREN'],
-    ['VALUE', '1'],
-    ['OPERATOR', '+'],
-    ['VALUE', '2'],
-    ['CLOSE_PAREN'],
+    [tokens.IDENTIFIER, 'PRINT'],
+    [tokens.OPEN_PAREN],
+    [tokens.NUMBER, '1'],
+    [tokens.OPERATOR, '+'],
+    [tokens.NUMBER, '2'],
+    [tokens.CLOSE_PAREN],
+    [tokens.STMT_END],
   ];
+  
   // Expected
   const ast = [
     {
