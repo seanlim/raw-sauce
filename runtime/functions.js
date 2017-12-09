9ee141f2ac1111e29c5b5123826cd36fb47843a1
Ravern Koh <ravern.koh.dev@gmail.com>
2017-12-09 12:16:36 +0800

    Added a couple more functions

--- runtime/functions.js
+++ runtime/functions.js
@@ -1,3 +1,5 @@
+const readlineSync = require('readline-sync');
+
 // Prints output to the console.
 module.exports.ITELLHER = {
   numArgs: 1,
@@ -6,7 +8,34 @@ module.exports.ITELLHER = {
   },
 };
 
+// Prints output to the console.
+module.exports.SENDMAN = {
+  numArgs: 1,
+  run: (prompt) => {
+    return readlineSync.question(prompt);
+  },
+};
+
+// Add two numbers.
 module.exports['+'] = {
   numArgs: 2,
   run: (l, r) => l + r,
+}
+
+// Subtract two numbers.
+module.exports['-'] = {
+  numArgs: 2,
+  run: (l, r) => l - r,
+}
+
+// Multiply two numbers.
+module.exports['*'] = {
+  numArgs: 2,
+  run: (l, r) => l * r,
+}
+
+// Divide two numbers.
+module.exports['/'] = {
+  numArgs: 2,
+  run: (l, r) => l / r,
 }
